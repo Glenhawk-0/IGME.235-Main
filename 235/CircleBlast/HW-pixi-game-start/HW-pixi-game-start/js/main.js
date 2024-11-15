ef25811363c00d6ef9edd038d64deb467c2930be
Lennon-0 <LennonEhare@Gmail.com>
2024-11-15 11:49:41 -0500

    Circle Blast 3 done (except for two chalenges and possible improvments)

--- 235/CircleBlast/HW-pixi-game-start/HW-pixi-game-start/js/main.js
+++ 235/CircleBlast/HW-pixi-game-start/HW-pixi-game-start/js/main.js
@@ -79,7 +79,7 @@ function setup() {
 	gameScene.addChild(ship);
 	// #6 - Load Sounds
 		shootSound  = new Howl({
-			src:['sounds']
+			src:['sounds/shoot.wav']
 		});
 
 		hitSound = new Howl ({
@@ -90,11 +90,13 @@ function setup() {
 			src: ['sounds/fireball.mp3']
 		});
 	// #7 - Load sprite sheet
+	explosionTextures = loadSpriteSheet();
 
 	// #8 - Start update loop
 	app.ticker.add(gameLoop);
 
 	// #9 - Start listening for click events on the canvas
+	app.view.onclick = fireBullet;
 
 	// Now our `startScene` is visible
 	// Clicking the button calls startGame()
@@ -270,12 +272,25 @@ function gameLoop(){
 	}
 	
 	// #4 - Move Bullets
-
+	for (let b of bullets){
+		b.move(dt);
+	}
 	
 	// #5 - Check for Collisions
 	for (let c of circles){
+		for(let b of bullets){
 		// #5A - circles & bullets
-		//TODO
+		if (rectsIntersect(c,b)){
+			fireballSound.play();
+			createExplosion(c.x,c.y,64,64); //we will implement this soon
+			gameScene.removeChild(c);
+			c.isAlive = false;
+			gameScene.removeChild(b);
+			b.isAlive = false;
+			increaseScoreBy(1);
+		}
+		if (b.y < -10) b.isAlive = false;
+	}
 
 		// #5B - circles & ship
 		if (c.isAlive && rectsIntersect(c,ship)){
@@ -286,6 +301,8 @@ function gameLoop(){
 		}
 	}
 	
+	// all done checking for collisions
+
 	// #6 - Now do some clean up
 	
 	// get rid of dead bullets
@@ -304,6 +321,10 @@ function gameLoop(){
 	}
 	
 	// #8 - Load next level
+	if (circles.length == 0){
+		levelNum ++;
+		loadLevel();
+	}
 }
 
 function createCircles(numCircles){
@@ -335,4 +356,50 @@ function end() {
 
 	gameOverScene.visible = true;
 	gameScene.visible = false;
+}
+
+function fireBullet(e){
+	//let rect = app.view.getBoundingClientRect();
+	//let mouseX = e.clientX - rect.x;
+	//let mouseY = e.clientY - rect.y;
+	//console.log(`${mouseX},${mouseY}`);
+	if (paused) return;
+
+	let b = new Bullet(0xFFFFFF, ship.x , ship.y);
+	bullets.push(b)
+	gameScene.addChild(b);
+	shootSound.play();
+}
+
+function loadSpriteSheet(){
+	// the 16 animation frames in each row are 64x64 pixels
+	// we are using the second row
+	// http://pixijs.download/release/docs/PIXI.BaseTexture.html
+	let spriteSheet = PIXI.BaseTexture.from("images/explosions.png");
+	let width = 64;
+	let height= 64;
+	let numFrames = 16;
+	let textures = [];
+	for (let i = 0; i<numFrames ; i++){
+		// http://pixijs.download/release/docs/PIXI.Texture.html
+		let frame = new PIXI.Texture(spriteSheet, new PIXI.Rectangle(i*width, 64, width, height));
+		textures.push(frame);
+	}
+	return textures;
+}
+ 
+function createExplosion(x, y, frameWidth, frameHeight){
+	// http://pixijs.download/release/docs/PIXI.AnimatedSprite.html
+	// the animation frames are 64x64 pixels
+	let w2 = frameWidth / 2;
+	let h2 = frameHeight / 2;
+	let expl = new PIXI.AnimatedSprite(explosionTextures);
+	expl.x = x - w2; // we want the explosions to appear at the center of the circle
+	expl.y = y - h2; // ditto
+	expl.animationSpeed = 1  / 7; 
+	expl.loop = false; 
+	expl.onComplete = e => gameScene.removeChild(expl);
+	explosions.push(expl)
+	gameScene.addChild(expl);
+	expl.play();
 }
